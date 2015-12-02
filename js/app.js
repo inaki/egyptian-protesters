573502749f1d565171774b9eb06f7fa76be926c6
Shokrof <mostafa.shokrof@gmail.com>
2015-12-02 10:09:22 +0200

    adding the count for attacked protests

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ angular.module('egyptApp', [])
   .controller('mainCtrl', ['$scope','$http','SheetItems', function($scope,$http,SheetItems) {
     
     $scope.byMonth = {"April": [],"May": [],"June": [],"July": [],"August": [],"September": [],"October": [],"November": []};
-
+      $scope.attacked_byMonth = {"April": 0,"May": 0,"June": 0,"July": 0,"August": 0,"September": 0,"October": 0,"November": 0};
     $scope.months = ["","January","February","March","April","May","June","July","August","September","October","November","December"];
 
     $attacked = 0; 
@@ -19,26 +19,50 @@ angular.module('egyptApp', [])
             switch(item.Month) {
                 case 4:
                     $scope.byMonth.April.push(item);
+		    if (item.Attacked=='TRUE'){
+		      $scope.attacked_byMonth.April+=1;
+                     }
                     break;
                 case 5:
+		if (item.Attacked=='TRUE'){
+		      $scope.attacked_byMonth.May+=1;
+                     }
                     $scope.byMonth.May.push(item);
                     break;
                 case 6:
+		if (item.Attacked=='TRUE'){
+		      $scope.attacked_byMonth.June+=1;
+                     }
                     $scope.byMonth.June.push(item);
                     break;
                 case 7:
+		if (item.Attacked=='TRUE'){
+		      $scope.attacked_byMonth.July+=1;
+                     }
                     $scope.byMonth.July.push(item);
                     break;
                 case 8:
+		if (item.Attacked=='TRUE'){
+		      $scope.attacked_byMonth.August+=1;
+                     }
                     $scope.byMonth.August.push(item);
                     break;
                 case 9:
+		if (item.Attacked=='TRUE'){
+		      $scope.attacked_byMonth.September+=1;
+                     }
                     $scope.byMonth.September.push(item);
                     break;
                 case 10:
+		if (item.Attacked=='TRUE'){
+		      $scope.attacked_byMonth.October+=1;
+                     }
                     $scope.byMonth.October.push(item);
                     break;
                 case 11:
+		if (item.Attacked=='TRUE'){
+		      $scope.attacked_byMonth.November+=1;
+                     }
                     $scope.byMonth.November.push(item);
                     break;
                 default:
